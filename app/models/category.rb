e202a372c21d0ea6c6b67582fd731c4a75a0f727
Lucas <lucas.pinilla@est.fib.upc.edu>
2021-05-16 03:12:25 +0200

    class categorie

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-    belongs_to :classroom
+    has_many :classroom
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_05_15_235514) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "tipo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
